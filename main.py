767ca3086a8608655cefe72cfd31b7b615f98908
Luka <lukadevrij@gmail.com>
2022-12-20 22:49:07 +0100

    Data extraction done

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     # Tesseract OCR
 
-        data = device.shell('screencap -p /sdcard/screen.png')
+        screenshot_data = device.shell('screencap -p /sdcard/screen.png')
         try:
             os.mkdir('tmp')
         except:
@@ -47,21 +47,44 @@ if __name__ == '__main__':
 
 
     img = cv2.imread('tmp/screen.png')
-    print(img.shape)  # Print image shape
     # Initial crop
-    cropped_image = img[920:1790, 40:885]
+    cropped_image = img[920:1795, 40:885]
+    # Print image shape
+    # Crop for better size
+    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
-    images = []
+    # mon_date = cropped_image[60:120, 10:150]
+    # cv2.imshow('', mon_date)
+    # cv2.waitKey(0)
+    # date_output = pytesseract.image_to_string(mon_date)
+    # print(date_output)
+
+    cropped_image = cv2.resize(cropped_image, (600, 700))
+    cropped_height = cropped_image.shape[0]
+
+    data = {}
+
+    dates = []
+    times = []
     prev = 0
     for i in range(7):
-        cropped = cropped_image[prev:math.floor((i+1) * (cropped_image.shape[1]) / 7), 40:885]
-        print(prev, math.floor((i+1) * (cropped_image.shape[1]) / 7))
-        prev = math.floor((i+1) * (cropped_image.shape[1]) / 7)
-        images.append(cropped)
-        cv2.imshow("pic", cropped)
-
-    cv2.waitKey(0)
+        # Divide into 7 days
+        day_crop = cropped_image[prev+5:int(cropped_height / 7) * (i + 1) - 5, 0:150]
+        date_ocr = pytesseract.image_to_string(day_crop)
+        date_split = date_ocr.split('\n')
+        dates.append(date_split[1])
 
 
-    #d = pytesseract.image_to_string(cropped_image, output_type=Output.DICT)
-    #print(d['text'])
+        # Divide into 7 days
+        time_crop = cropped_image[prev + 5:int(cropped_height / 7) * (i + 1) - 5, 170:400]
+        time_ocr = pytesseract.image_to_string(time_crop)
+        time_trim = time_ocr[0:len(time_ocr)-1]
+        times.append(time_trim)
+
+        data[date_split[1]] = time_trim
+
+        prev = int(cropped_height / 7) * (i + 1)
+
+    print(data)
+
+
